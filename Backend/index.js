d9b1d7fca703eed89cc7db54a493e5209ae2ddaf
Gitesh Makwane <111109845+giteshmakwane@users.noreply.github.com>
2025-11-28 20:45:21 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -6,31 +6,31 @@ import cors from "cors";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
+dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
-
 const PORT = process.env.PORT || 4000;
 const URI = process.env.MongoDBURI;
 
 // connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
+const connectDB = async () => {
+  try {
+    await mongoose.connect(URI);
     console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+  } catch (error) {
+    console.log("MongoDB connection error:", error.message);
+  }
+};
 
-// defining routes
+connectDB();
+
+// routes
 app.use("/book", bookRoute);
 app.use("/user", userRoute);
 
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+  console.log(`Server is listening on port ${PORT}`);
 });
